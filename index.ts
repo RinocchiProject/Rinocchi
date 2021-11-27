89ba0153c206bb27dbe70059ca5a3e37d3d50c9d
CloudyyUw <cloudyy.contact@gmail.com>
2021-11-27 00:32:37 -0300

    :construction: WIP

---
+++ index.ts
@@ -0,0 +1,11 @@
+import { readFileSync } from 'fs';
+import Parse from './src/util/ConfigParser';
+import Client from './src/core/Client';
+
+const content = readFileSync('./CONFIG', 'utf-8'),
+    obj = Object.assign({}, ...Parse(content));
+
+global.settings = obj;
+
+import { test } from './src/util/ConfigParser';
+test();
