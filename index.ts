67b5fc0ab04511fea2361d762a58c38deaf93463
CloudyyUw <cloudyy.contact@gmail.com>
2021-12-09 22:27:52 -0300

    :construction: Adicionando suporte a sharding

--- index.ts
+++ index.ts
@@ -1,8 +1,16 @@
 import { readFileSync } from 'fs';
 import Parse from './src/util/ConfigParser';
-import Client from './src/core/Client';
+import { ShardingManager } from 'discord.js';
 
 const content = readFileSync('./CONFIG', 'utf-8'),
     obj = Object.assign({}, ...Parse(content));
 
 global.settings = obj;
+
+// Sharding
+new ShardingManager('./build/main.js', {
+    token: obj.TOKEN,
+    totalShards: 'auto',
+}).on('shardCreate', (shard) => {
+    console.log(`Shard # ${shard.id + 1}`);
+});
