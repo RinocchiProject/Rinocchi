302b9e8db9a3842f3c419fc309d35deab66c5ec4
CloudyyUw <cloudyy.contact@gmail.com>
2021-12-09 23:10:41 -0300

    :bug: :sparkles: :construction: Auto Login

--- src/core/Client.ts
+++ src/core/Client.ts
@@ -1,8 +1,9 @@
-import { Client, Collection, ClientOptions } from 'discord.js';
+import { Client, ClientOptions } from 'discord.js';
 import Locale from './Locales';
 import Logs from '../util/Logger';
 import CommandManager from './CommandManager';
 import EventsManager from './EventsManager';
+import { obj as Settings } from '../util/ConfigParser';
 
 class Bot extends Client {
     public lang: Locale;
@@ -10,19 +11,20 @@ class Bot extends Client {
     public commands: CommandManager;
     public events: EventsManager;
     public temp: Map<string, any> = new Map();
-    constructor(options: ClientOptions) {
+    private settings: typeof Settings = Settings;
+    constructor(options?: ClientOptions) {
         super(options);
         this.lang = new Locale();
         this.log = new Logs();
         this.events = new EventsManager(this);
         this.commands = new CommandManager(this);
     }
-    public async login(token: string): Promise<string> {
+    public async login(): Promise<string> {
         await this.lang.init();
-        this.commands.loadPath('./src/commands');
-        this.events.loadPath('./src/events');
+        // this.commands.loadPath('./build/src/commands');
+        this.events.loadPath('./build/src/events');
         this.log.ok('OK');
-        return super.login(token);
+        return super.login(this.settings.TOKEN);
     }
 }
 
