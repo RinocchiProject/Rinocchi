c70d2896ab1e88a5d1d209fa9647933e49adf618
CloudyyUw <cloudyy.contact@gmail.com>
2021-12-22 23:11:24 -0300

    :sparkles: Suporte a slash commands

--- src/commands/info/avatar.ts
+++ src/commands/info/avatar.ts
@@ -1,35 +1,36 @@
 import Command from '../../core/Command';
-import Context from '../../types/CommandContext';
 import Request from '../../util/getUser';
-import ParseMention from '../../util/getUserFromMention';
+import Client from '../../core/Client';
 import ApiUserType from '../../types/UserApiType';
 import { MessageEmbed } from 'discord.js';
+import { SlashCommandBuilder } from '@discordjs/builders';
+import Context from '../../types/CommandContext';
 
 export default class Avatar extends Command {
     public name: string = 'avatar';
-    public aliases: string[] = ['a'];
     public dev: boolean = false;
-    public help(context: Context): void {
-        let { message, locale } = context;
-        message.reply(
-            locale('help:avatar', {
-                example_mention: 'avatar @user',
-                example_id: 'avatar 12345',
-            })
+    public data = new SlashCommandBuilder()
+        .setName('avatar')
+        .setDescription(
+            'Get the avatar of any discord user (by ID) or by mention'
+        )
+        .addUserOption((option) =>
+            option
+                .setName('user')
+                .setDescription('Mention the user or enter an ID')
+                .setRequired(false)
         );
-    }
     public async run(context: Context): Promise<void> {
         let { client, message, args, locale } = context,
             user: any =
-                ParseMention(client, args[0]) ||
                 (await Request.obj(args[0], client.settings.TOKEN)) ||
-                message.author,
+                message.user,
             apiUser: ApiUserType = await Request.obj(
                 user.id,
                 client.settings.TOKEN
             );
         if (!apiUser.id) {
-            message.reply('commands:avatar.unknow_user');
+            message?.followUp('commands:avatar.unknow_user');
             return;
         }
         let emb = new MessageEmbed()
@@ -42,6 +43,6 @@ export default class Avatar extends Command {
             .setImage(
                 await Request.avatar(apiUser.id, 2048, client.settings.TOKEN)
             );
-        message.reply({ embeds: [emb] });
+        message?.followUp({ embeds: [emb] });
     }
 }
