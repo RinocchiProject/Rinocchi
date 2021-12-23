be3830116266f79ac47410e0a5db4a504f48231f
CloudyyUw <cloudyy.contact@gmail.com>
2021-12-22 23:20:43 -0300

    :rotating_light: Testes

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
 import Client from './src/core/Client';
 import Server from './src/server/main';
+import { REST } from '@discordjs/rest';
+import { Routes } from 'discord-api-types/v9';
 
 const client = new Client({
     intents: [
@@ -19,5 +21,25 @@ const client = new Client({
     ],
 });
 client.login();
+const rest = new REST({ version: '9' }).setToken(client.settings.TOKEN);
 
+function GetCmds() {
+    return new Promise((resolve, _) => {
+        let arr = [];
+        client.commands.modules.forEach((x) => {
+            arr.push(x.data.toJSON());
+        });
+        resolve(arr);
+    });
+}
+client.on('ready', async () => {
+    let commands = await GetCmds();
+    await rest.put(
+        Routes.applicationGuildCommands(
+            client.user.id,
+            client.settings.TESTGUILD
+        ),
+        { body: commands }
+    );
+});
 client.on('ready', () => Server(client));
