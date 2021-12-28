8f9821d7bf9389eae3512c18283e77fd018a494f
CloudyyUw <cloudyy.contact@gmail.com>
2021-12-28 18:15:40 -0300

    :sparkles: Adicionando a tabela backups

--- database.sql
+++ database.sql
@@ -8,11 +8,6 @@ CREATE TABLE IF NOT EXISTS user_balance (
   val int NOT NULL
 )
 
-CREATE TABLE IF NOT EXISTS guild_prefix (
-  id varchar(50) NOT NULL PRIMARY KEY,
-  prefix varchar(3) NOT NULL
-)
-
 CREATE TABLE IF NOT EXISTS blacklist_user (
   id varchar(50) NOT NULL PRIMARY KEY,
   ban BOOLEAN
@@ -21,4 +16,9 @@ CREATE TABLE IF NOT EXISTS blacklist_user (
 CREATE TABLE IF NOT EXISTS blacklist_guild (
   id varchar(50) NOT NULL PRIMARY KEY,
   ban BOOLEAN
+)
+
+CREATE TABLE IF NOT EXISTS backups (
+  id varchar(100) NOT NULL PRIMARY KEY,
+  url varchar(255) NOT NULL
 )
