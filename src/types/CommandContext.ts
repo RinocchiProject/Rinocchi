bcad6b045ea20d53a79bef28688a539be906d926
CloudyyUw <cloudyy.contact@gmail.com>
2022-01-03 00:53:20 -0300

    :sparkles: Adicionando RawOptions

--- src/types/CommandContext.ts
+++ src/types/CommandContext.ts
@@ -1,20 +1,29 @@
 import Client from '../core/Client';
 import { CommandInteraction } from 'discord.js';
 
+interface RawOption {
+    name: string;
+    type: string;
+    value?: any;
+}
+
 export default class Context {
     public client: Client;
     public message: CommandInteraction;
     public args: string[];
     public locale: any;
+    public rawOptions: Readonly<RawOption[]>;
     constructor(
         client: Client,
         message: CommandInteraction,
         args: string[],
-        locale: any
+        locale: any,
+        raw: Readonly<RawOption[]>
     ) {
         this.client = client;
         this.args = args;
         this.locale = locale;
         this.message = message;
+        this.rawOptions = raw;
     }
 }
