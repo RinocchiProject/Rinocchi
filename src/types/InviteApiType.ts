d389ce5768a50c62f839c270ab0ae5552125bb7c
CloudyyUw <cloudyy.contact@gmail.com>
2022-01-04 20:11:28 -0300

    :recycle: Reduzindo a interface

--- src/types/InviteApiType.ts
+++ src/types/InviteApiType.ts
@@ -1,31 +1,29 @@
 import User from './UserApiType';
 
-interface WelcomeScreen {
-    description: string;
-    welcome_channels: [
-        {
-            description: string;
-            emoji_name: string;
-            emoji_id: string;
-        }
-    ];
-}
-interface PartialGuild {
-    id: string;
-    name: string;
-    icon: string;
-    banner?: string;
-    splash?: string;
-    description?: string;
-    features?: string[];
-    vanity_url_code?: string;
-    welcome_screen?: WelcomeScreen;
-    nsfw: boolean;
-    nsfw_level: number;
-}
 interface Invite {
     code: string;
-    guild?: PartialGuild;
+    guild?: {
+        id: string;
+        name: string;
+        icon: string;
+        banner?: string;
+        splash?: string;
+        description?: string;
+        features?: string[];
+        vanity_url_code?: string;
+        welcome_screen?: {
+            description: string;
+            welcome_channels: [
+                {
+                    description: string;
+                    emoji_name: string;
+                    emoji_id: string;
+                }
+            ];
+        };
+        nsfw: boolean;
+        nsfw_level: number;
+    };
     inviter?: User;
     approximate_presence_count?: number;
     approximate_member_count?: number;
